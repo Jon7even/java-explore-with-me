12922dee8efec074d65328b4947bf13effebe9be
JonSeven <mazahaker5555@yandex.ru>
2023-09-02 23:12:12 +0300

    fix

--- stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/RequestStatListTO.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/RequestStatListTO.java
@@ -1,11 +1,10 @@
 package ru.practicum.ewm.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-import lombok.Builder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,7 +22,6 @@ public class RequestStatListTO {
     @JsonFormat(pattern = DATE_TIME_HIT)
     private LocalDateTime end;
 
-    @NotNull
     private List<String> uris;
 
     private boolean unique;
