3980e8519513b0c3b97be3135d49ac62edf92c1e
JonSeven <mazahaker5555@yandex.ru>
2023-09-02 23:07:11 +0300

    fix: NotNull

--- stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/RequestStatListTO.java
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/RequestStatListTO.java
@@ -1,10 +1,11 @@
 package ru.practicum.ewm.stats.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,6 +23,7 @@ public class RequestStatListTO {
     @JsonFormat(pattern = DATE_TIME_HIT)
     private LocalDateTime end;
 
+    @NotNull
     private List<String> uris;
 
     private boolean unique;
